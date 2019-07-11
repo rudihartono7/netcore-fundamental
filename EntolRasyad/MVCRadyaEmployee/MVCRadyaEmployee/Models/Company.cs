001b89d8b52f5eb64b5eb82fca3aecae66d8f721
entolrasyad <rasyad01@gmail.com>
2019-07-11 14:45:36 +0700

    add project web data karyawan

---
+++ EntolRasyad/MVCRadyaEmployee/MVCRadyaEmployee/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCRadyaEmployee.Models
+{
+    public class Company
+    {
+        public int CompanyID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public ICollection<Employee> Team { get; set; }
+    }
+}
