540dcaa75e5f48b274c122c410b40bab4d73603a
Levs <Levs@DESKTOP-T29N920>
2019-07-15 17:20:43 +0700

    test

---
+++ Kovalevshero/KovalevsheroMVC2/Controllers/EmployeesAPIController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KovalevsheroMVC.Models;
+
+namespace KovalevsheroMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesAPIController : ControllerBase
+    {
+        private readonly KovalevsheroMVCContext _context;
+
+        public EmployeesAPIController(KovalevsheroMVCContext context)
+        {
+            _context = context;
+
+            if (_context.Employee.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.Employee.Add(new Employee { Id = 1, Name = "Levs", Position = "Master", CompanyID = 1 });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetTodoItems()
+        {
+            return await _context.Employee.ToListAsync();
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetTodoItem(int id)
+        {
+            var todoItem = await _context.Employee.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+
+    }
+}
