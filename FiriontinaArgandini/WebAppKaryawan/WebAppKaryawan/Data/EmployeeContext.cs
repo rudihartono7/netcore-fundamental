aa0cf83ce84ad68bdaf613f34f9bcd74a8ee93fd
firiontinaa <firiontina.argan.tif17@polban.ac.id>
2019-07-15 16:41:18 +0700

    Web Data Karyawan dan Perusahaan (MVC)

---
+++ FiriontinaArgandini/WebAppKaryawan/WebAppKaryawan/Data/EmployeeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAppKaryawan.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext (DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppKaryawan.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ FiriontinaArgandini/WebAppKaryawan/WebAppKaryawan/Models/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppKaryawan.Models
+{
+    public class Company
+    {
+        public int CompanyID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
