b9a90fc0b268db6c3180f079a769bd8fcf7aabf2
Levs <Levs@DESKTOP-T29N920>
2019-07-15 16:52:37 +0700

    Add project MVC dan API data Employee

---
+++ Kovalevshero/KovalevsheroWebAPI/KovalevsheroWebAPI/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KovalevsheroWebAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> TodoItems { get; set; }
+    }
+}
